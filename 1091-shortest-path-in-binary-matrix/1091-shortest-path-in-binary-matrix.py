52f555206a4efc12fa0da471aef80a743f7fd153
queenJiJi <lovebellaberry52@gmail.com>
2024-05-23 00:41:20 +0900

    Time: 501 ms (52.06%), Space: 11.9 MB (84.51%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,30 +1,29 @@
 from collections import deque
 class Solution(object):
     def shortestPathBinaryMatrix(self, grid):
-        row_len,col_len = len(grid), len(grid[0])
-        visited= [[False]*col_len for _ in range(row_len)]
-        dr = [-1,1,0,0,-1,-1,1,1]
-        dc = [0,0,-1,1,-1,1,-1,1]
-        def bfs(r,c,path):
-            q = deque()
-            q.append((r,c,path))
-            visited[r][c] = True
+       q=deque()
+       q.append((0,0,1))
+       n = len(grid)
+       visited=[[False]*n for _ in range(n)]
 
-            if grid[0][0]==1 or grid[row_len-1][col_len-1] == 1:
+       while q:
+            cur_r,cur_c,path = q.popleft()
+            visited[cur_r][cur_c] = True
+
+            if cur_r==0 and cur_c==0 and grid[cur_r][cur_c] == 1:
                 return -1
+            
+            if (cur_r,cur_c) == (n-1, n-1):
+                return path            
+            
+            for dr,dc in [[-1,0],[1,0],[0,1],[0,-1],[1,1],[1,-1],[-1,1],[-1,-1]]:
+                next_r, next_c = cur_r+dr, cur_c+dc
+                if 0<=next_r<n and 0<=next_c<n and grid[next_r][next_c] == 0:
+                    if not visited[next_r][next_c]:
+                        q.append((next_r,next_c,path+1))
+                        visited[next_r][next_c] = True
+       return -1
+    
+       
 
-            while q:
-                cur_r,cur_c,cur_path = q.popleft()
-
-                if (cur_r,cur_c) == (row_len-1, col_len-1):
-                    return cur_path
-                
-                for i in range(8):
-                    next_r,next_c =cur_r+dr[i], cur_c+dc[i]
-                    if 0<=next_r<row_len and 0<=next_c<col_len and grid[next_r][next_c] == 0:
-                        if not visited[next_r][next_c]:
-                            q.append((next_r,next_c,cur_path+1))
-                            visited[next_r][next_c] = True
-            return -1
-        return bfs(0,0,1)
                 
