72635b9860fab14fd0b49a827ba04c232f62d773
queenJiJi <lovebellaberry52@gmail.com>
2024-05-27 19:11:28 +0900

    Time: 945 ms (25.16%), Space: 63.5 MB (83.48%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,19 +1,28 @@
 class Solution(object):
     def combine(self, n, k):
-        def backtrack(start, cur, result):
-            if len(cur) == k:
-                result.append(cur[:])
-                return
+        # def backtrack(start, cur, result):
+        #     if len(cur) == k:
+        #         result.append(cur[:])
+        #         return
 
-            for i in range(start, n+1):
-                cur.append(i)
-                backtrack(i+1,cur,result)
-                cur.pop()
+        #     for i in range(start, n+1):
+        #         cur.append(i)
+        #         backtrack(i+1,cur,result)
+        #         cur.pop()
 
+        #     return result
+
+        # return backtrack(1,[],[])
+
+        ans,result = [],[]
+        def backtrack(start):
+            if len(ans)==k:
+                result.append(ans[:])
+                return 
+            
+            for i in range(start,n+1):
+                ans.append(i)
+                backtrack(i+1)
+                ans.pop()
             return result
-
-        return backtrack(1,[],[])
-
-
-
-        
+        return backtrack(1)
