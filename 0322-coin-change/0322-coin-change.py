6965a55959cb510b583d6d7f978f92de98bfd57e
queenJiJi <lovebellaberry52@gmail.com>
2024-05-29 22:21:25 +0900

    Time: 384 ms (98.29%), Space: 12.6 MB (26.42%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,17 +1,20 @@
 from collections import deque
 class Solution(object):
     def coinChange(self, coins, amount):
-        q= deque([(amount,0)])
+        q= deque()
+        q.append((amount,0)) # 할당가격,사용 코인
         visited= set()
         while q:
-            cur_amount, num_coin = q.popleft()
-            
+            cur_amount, num_coin = q.popleft()  
+            visited.add(cur_amount)
+
             if cur_amount == 0:
                 return num_coin
-
+            
             for coin in coins:
-                next_coin = cur_amount - coin
-                if next_coin not in visited and next_coin>=0:
-                    q.append((next_coin, num_coin+1))
-                    visited.add(next_coin)
+                next_amount = cur_amount - coin
+                if next_amount not in visited and next_amount >=0:
+                    q.append((next_amount,num_coin+1))
+                    visited.add(next_amount)
+            
         return -1
