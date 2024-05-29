9b9af3eebb3801ac5a0d21966ea0a70d31dc7f41
queenJiJi <lovebellaberry52@gmail.com>
2024-05-29 23:32:06 +0900

    Time: 116 ms (98.88%), Space: 11.9 MB (88.1%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,28 +1,49 @@
 from collections import deque
 class Solution(object):
     def isBipartite(self, graph):
-        visited = [0] * len(graph)
-        q = deque()
-        def bfs(start_v):
-            if visited[start_v]:
-                return True
+        # visited = [0] * len(graph)
+       
+        # def dfs(cur_v):
+        #     for next_v in graph[cur_v]:
+        #         if visited[next_v] == 0:
+        #             visited[next_v] = -1*visited[cur_v]
+        #             if not dfs(next_v):
+        #                 return False
+        #         if visited[next_v] == visited[cur_v]:
+        #             return False
+        #     return True
+        
+        # for i in range(len(graph)):
+        #     if visited[i]==0:
+        #         visited[i] = 1
+        #         if not dfs(i):
+        #             return False
+        # return True
+        n = len(graph)
+        visited= [0] * n
 
-            q.append(start_v)    
-            visited[start_v] = -1
+        q=deque()
+
+        def bfs(start_v):
+            q.append(start_v)
+            visited[start_v] = 1
+
             while q:
                 cur_v = q.popleft()
-                
+
                 for next_v in graph[cur_v]:
-                    if visited[next_v] == visited[cur_v]:
+                    if visited[next_v]== visited[cur_v]:
                         return False
-                    elif not visited[next_v]:
+                    elif visited[next_v] == 0:
                         visited[next_v] = -1*visited[cur_v]
                         q.append(next_v)
             return True
-        
-        for i in range(len(graph)):
-            if not bfs(i):
-                return False
+
+        for i in range(n):
+            if visited[i]==0:
+                visited[i] = 1
+                if not bfs(i):
+                    return False
         return True
                 
 
