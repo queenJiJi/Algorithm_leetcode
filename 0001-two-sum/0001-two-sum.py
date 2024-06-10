300054ecddf7f0b92cb4f4acaa068030e26089b4
queenJiJi <lovebellaberry52@gmail.com>
2024-06-10 17:48:19 +0900

    Time: 44 ms (59.83%), Space: 12.6 MB (43.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,20 +1,12 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        nums = [[idx,val] for idx,val in enumerate(nums)]
-        nums.sort(key=lambda x:x[1])
-        l,r = 0,len(nums)-1
-
-        while l<r:
-            two_sum = nums[l][1]+nums[r][1]
-            if two_sum>target:
-                r-=1
-            elif two_sum<target:
-                l+=1
-            else:
-                return [nums[l][0], nums[r][0]]
-        return False
-
-        
+        hash = {}
+        n = len(nums)
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in hash:
+                return [hash[complement], i]
+            hash[nums[i]] = i        
 
             
                   
