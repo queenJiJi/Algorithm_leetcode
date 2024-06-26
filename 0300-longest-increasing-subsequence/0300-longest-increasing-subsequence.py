e6e211571d8cf0414a2039dd3c7f0b8d79c4408e
queenJiJi <lovebellaberry52@gmail.com>
2024-06-26 20:37:14 +0900

    Time: 2042 ms (55.58%), Space: 11.8 MB (96.68%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,10 +1,31 @@
+from bisect import bisect_left
 class Solution(object):
     def lengthOfLIS(self, nums):
-        dp= [1]* len(nums) # 아무리 길이가 짧아도 본인 길이1일테니까 dp테이블의 원소를 모두 1로 초기화
-
-        for i in range(1,len(nums)):
+        n = len(nums)
+        dp = [1] * n
+        for i in range(1,n):
             for j in range(0,i):
-                if nums[i]>nums[j]:
+                if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j]+1)
+
         return max(dp)
+        # SOL 1- DP
+        # dp= [1]* len(nums) # 아무리 길이가 짧아도 본인 길이1일테니까 dp테이블의 원소를 모두 1로 초기화
+
+        # for i in range(1,len(nums)):
+        #     for j in range(0,i):
+        #         if nums[i]>nums[j]:
+        #             dp[i] = max(dp[i], dp[j]+1)
+        # return max(dp)
         
+        # SOL 2- binary seacrh
+
+        # ans = []
+        # for num in nums:
+        #     index = bisect_left(ans,num)
+
+        #     if index == len(ans):
+        #         ans.append(num)
+        #     else:
+        #         ans[index] = num
+        # return len(ans)
