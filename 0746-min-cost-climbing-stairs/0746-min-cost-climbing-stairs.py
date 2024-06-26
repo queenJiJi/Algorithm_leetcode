41119533997d6edae11a967de2b9f9d51687dd4b
queenJiJi <lovebellaberry52@gmail.com>
2024-06-26 19:54:03 +0900

    Time: 29 ms (88.9%), Space: 11.7 MB (77.05%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,18 +1,53 @@
 class Solution(object):
     def minCostClimbingStairs(self, cost):
         n = len(cost)
+        dp = [float('inf')] * (n+1)
+
+        dp[0] = 0
+        dp[1] = 0
+        
+        for i in range(2,n+1):
+            dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+
+        return dp[n]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # n = len(cost)
        # top-down
-        memo = {}
+        # memo = {}
 
-        def dp(n):
-            if n==0 or n==1:
-                return 0
+        # def dp(n):
+        #     if n==0 or n==1:
+        #         return 0
             
-            if n not in memo:
-                memo[n] = min(dp(n-1)+cost[n-1], dp(n-2)+cost[n-2])
-            return memo[n]
-        return dp(n)
+        #     if n not in memo:
+        #         memo[n] = min(dp(n-1)+cost[n-1], dp(n-2)+cost[n-2])
+        #     return memo[n]
+        # return dp(n)
+        
+        # bottom-up
+        dp = [-1] * (n+1)
 
+        dp[0] = 0
+        dp[1] = 0
+
+        for i in range(2,n+1):
+            dp[i] = min(dp[i-1]+cost[i-1] , dp[i-2]+cost[i-2])
+        return dp[n]
+        
 
 
         
