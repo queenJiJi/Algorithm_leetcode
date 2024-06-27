9b733b4c55f18e8bd11977753279f7eff25251e0
queenJiJi <lovebellaberry52@gmail.com>
2024-06-28 00:00:28 +0900

    Time: 559 ms (52.44%), Space: 25.9 MB (85.37%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -6,27 +6,26 @@ class Solution(object):
         for i in range(len(edges)):
             edges[i].append(succProb[i])
         
-        for a,b,w in edges:
-            graph[a].append((w,b))
-            graph[b].append((w,a))
+        for u,v,w in edges:
+            graph[u].append([w,v])
+            graph[v].append([w,u])
 
-        # print(graph)
-
-  
-
-        prob = [0.0]*n
-        pq = [(-1.0,start_node)]
+        
+        pq = [(-1.0, start_node)]
+        prob = [0.0]*(n)
         prob[start_node] = 1.0
+
         while pq:
-            cur_prob, cur_node = heapq.heappop(pq)
-            # if prob[cur_node] > cur_prob:
-            #     continue
+            cur_prob, cur_v = heapq.heappop(pq)
+            if cur_v == end_node:
+                return prob[end_node]
             
-            for probability, next_node in graph[cur_node]:
-                next_probability = -probability*cur_prob
-                if next_probability > prob[next_node]:
-                    prob[next_node] = next_probability
-                    heapq.heappush(pq,(-next_probability,next_node))
-        # print(prob)
-        return prob[end_node]
+            for proba, next_v in graph[cur_v]:
+                next_proba = (-proba * cur_prob)
+
+                if next_proba>prob[next_v]:
+                    heapq.heappush(pq,(-next_proba, next_v))
+                    prob[next_v] =next_proba
+
+        return 0
         
