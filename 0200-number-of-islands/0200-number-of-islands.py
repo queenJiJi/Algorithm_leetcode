0c3e680a58617b61b2910e8e6ee2b62532ecf096
queenJiJi <lovebellaberry52@gmail.com>
2024-07-05 23:09:24 +0900

    Time: 223 ms (59.41%), Space: 28.2 MB (46.43%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,37 +1,28 @@
-import collections
+from collections import deque
 class Solution(object):
     def numIslands(self, grid):
-        q = collections.deque()
-        m,n = len(grid), len(grid[0])
-        visited = [[False]*n for _ in range(m)]
         count = 0
+        row_len,col_len = len(grid), len(grid[0])
+        visited = [[False]*col_len for _ in range(row_len)]
+        q= deque()
 
-        def dfs(r,c):
+        def bfs(r,c):
+            q.append((r,c))
             visited[r][c] = True
-            for dr,dc in [[-1,0],[1,0],[0,-1],[0,1]]:
-                next_r,next_c = r+dr, c+dc
-                if 0<=next_r<m and 0<=next_c<n and grid[next_r][next_c]=='1':
-                    if not visited[next_r][next_c]:
-                        dfs(next_r,next_c)
 
-        # def bfs(r,c):
-        #     q.append((r,c))
-        #     while q:
-        #         cur_r,cur_c = q.popleft()
-        #         visited[cur_r][cur_c] = True
-                
-        #         for dr,dc in [[-1,0],[1,0],[0,-1],[0,1]]:
-        #             next_r,next_c = cur_r+dr, cur_c+dc
-        #             if 0<=next_r<m and 0<=next_c<n and grid[next_r][next_c]=='1':
-        #                 if not visited[next_r][next_c]:
-        #                     q.append((next_r,next_c))
-        #                     visited[next_r][next_c] = True
+            while q:
+                cur_r,cur_c = q.popleft()
+            
+                for dr,dc in [[-1,0],[1,0],[0,-1],[0,1]]:
+                    nr,nc = cur_r+dr, cur_c+dc
+                    if 0<=nr<row_len and 0<=nc<col_len and grid[nr][nc]=='1' and not visited[nr][nc]:
+                        q.append((nr,nc))
+                        visited[nr][nc] =True
 
-        for r in range(m):
-            for c in range(n):
+        for r in range(row_len):
+            for c in range(col_len):
                 if grid[r][c] == '1' and not visited[r][c]:
-                    # bfs(r,c)
-                    dfs(r,c)
+                    bfs(r,c)
                     count+=1
         return count
 
