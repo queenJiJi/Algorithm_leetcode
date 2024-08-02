17e5d482748eb71eca51ccd2473652f8e922bc1d
queenJiJi <lovebellaberry52@gmail.com>
2024-08-02 16:07:31 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,28 @@
-import collections
+from collections import deque
 class Solution(object):
     def numIslands(self, grid):
-        m,n = len(grid), len(grid[0])
-        visited= [[False]*n for _ in range(m)]
-        count =0 
-        def dfs(r,c):
-            visited[r][c] = True
-            for dr,dc in [[-1,0],[0,1],[1,0],[0,-1]]:
-                next_r,next_c = dr+r,dc+c
-                if 0<=next_r<m and 0<=next_c<n and not visited[next_r][next_c] and grid[next_r][next_c] == '1':
-                    dfs(next_r,next_c)
+        def bfs(i,j):
+            q = deque()
+            q.append((i,j))
+            visited[i][j] == True
 
-
-        
-        for r in range(m):
-            for c in range(n):
-                if grid[r][c] == '1' and not visited[r][c]:
-                    dfs(r,c)
-                    count+=1
-        return count
+            while q:
+                cur_r,cur_c = q.popleft()
+                
+                for dr,dc in [[-1,0],[1,0],[0,-1],[0,1]]:
+                    nr,nc = cur_r+dr, cur_c+dc
+                    if 0<=nr<n and 0<=nc<m and not visited[nr][nc] and grid[nr][nc] == '1':
+                        q.append((nr,nc))
+                        visited[nr][nc] = True
+        n,m=len(grid),len(grid[0])
+        answer = 0
+        visited= [[False]*m for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == '1' and not visited[i][j]:
+                    bfs(i,j)
+                    answer +=1
+        return answer
        
                     
 
