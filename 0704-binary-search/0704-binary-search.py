55efcb2383623a71b7407570ae3bb4bcd3802eb1
queenJiJi <lovebellaberry52@gmail.com>
2024-10-09 20:20:46 +0900

    Time: 180 ms (62.89%), Space: 12.9 MB (47.77%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,19 +1,26 @@
 from bisect import bisect_left
 class Solution(object):
     def search(self, nums, target):
+        answer = bisect_left(nums,target)
+        if answer!=len(nums) and target == nums[answer]:
+            return answer
+        return -1
+
+
+
         # ans = bisect_left(nums,target)
         # if ans!=len(nums) and target == nums[ans]:
         #     return ans
         # return -1
 
-        l,r = 0,len(nums)-1
+        # l,r = 0,len(nums)-1
         
-        while l<=r:
-            mean = (l+r)//2
-            if nums[mean] ==target:
-                return mean
-            elif nums[mean]> target:
-                r = mean-1
-            else:
-                l = mean+1
-        return -1
+        # while l<=r:
+        #     mean = (l+r)//2
+        #     if nums[mean] ==target:
+        #         return mean
+        #     elif nums[mean]> target:
+        #         r = mean-1
+        #     else:
+        #         l = mean+1
+        # return -1
