8863a7250bf66eb049655cc836b80390acec204f
queenJiJi <lovebellaberry52@gmail.com>
2024-10-10 04:14:55 +0900

    Time: 8533 ms (6.5%), Space: 11.7 MB (42.14%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,22 +2,27 @@ from collections import deque
 
 class Solution(object):
     def exist(self, board, word):
-        n,m = len(board),len(board[0])
+        
+        n,m = len(board), len(board[0])
         visited = set()
-        def dfs(r,c,idx):
 
+        def dfs(r,c,idx):
+        
             if len(word) == idx:
                 return True
-            
-            if not (0<=r<n and 0<=c<m) or (r,c) in visited or word[idx] != board[r][c]:
+
+            if not (0<=r<n and 0<=c<m and (r,c) not in visited and word[idx] == board[r][c]):
                 return False
 
             visited.add((r,c))
-            for dr,dc in [[-1,0],[1,0],[0,-1],[0,1]]:
-                nr,nc = r+dr, c+dc
+            
+            for dx,dy in [[-1,0],[0,1],[0,-1],[1,0]]:
+                nr,nc = r+dx, c+dy
+
                 if dfs(nr,nc,idx+1):
                     return True
             visited.remove((r,c))
+
 
         for r in range(n):
             for c in range(m):
