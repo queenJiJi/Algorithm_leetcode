bf740be46861809bc090ec729799800f3460f1ea
queenJiJi <lovebellaberry52@gmail.com>
2024-10-10 04:29:12 +0900

    Time: 5162 ms (83.97%), Space: 11.6 MB (72.2%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,36 +1,28 @@
 class Solution(object):
     def exist(self, board, word):
         n,m = len(board), len(board[0])
-        visited = set()
         
         def dfs(r,c,idx):
-            
             if len(word) == idx:
                 return True
 
-            if not (0<=r<n and 0<=c<m and (r,c) not in visited and word[idx]==board[r][c]):
+            # 경계 벗어나거나 현재 문자가 일치하지 않는 경우
+            if r<0 or r>=n or c<0 or c>=m or word[idx] !=board[r][c]:
                 return False
 
-            visited.add((r,c))
+            tmp = board[r][c] # 방문한 셀을 임시로 마킹
+            board[r][c] = '#' # 방문 표시
 
-            for dx,dy in [[-1,0],[1,0],[0,-1],[0,1]]:
-                nr,nc = r+dx, c+dy
-                if dfs(nr,nc,idx+1):
-                    return True
+            # 상하좌우탐색
+            find = (dfs(r+1,c, idx+1) or dfs(r,c+1,idx+1) or dfs(r-1,c,idx+1) or dfs(r,c-1,idx+1))
             
-            visited.remove((r,c))
+            board[r][c] = tmp # 백트래킹: 탐색 후 원래 값으로 복원
+            return find
+            
 
-        
         for r in range(n):
             for c in range(m):
                 if dfs(r,c,0):
                     return True
         return False
-                
-
-
-
-        
-
-
         
