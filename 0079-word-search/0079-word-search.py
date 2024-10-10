d0f3a51174ae70cffd6be776e41a2787da1b7918
queenJiJi <lovebellaberry52@gmail.com>
2024-10-10 14:33:39 +0900

    Time: 8533 ms (6.69%), Space: 11.7 MB (41.84%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,28 +1,31 @@
+from collections import deque
+
 class Solution(object):
     def exist(self, board, word):
-        n,m = len(board), len(board[0])
         
+        n,m = len(board), len(board[0])
+        visited = set()
+
         def dfs(r,c,idx):
+        
             if len(word) == idx:
                 return True
 
-            # 경계 벗어나거나 현재 문자가 일치하지 않는 경우
-            if r<0 or r>=n or c<0 or c>=m or word[idx] !=board[r][c]:
+            if not (0<=r<n and 0<=c<m and (r,c) not in visited and word[idx] == board[r][c]):
                 return False
 
-            tmp = board[r][c] # 방문한 셀을 임시로 마킹
-            board[r][c] = '#' # 방문 표시
+            visited.add((r,c))
+            
+            for dx,dy in [[-1,0],[0,1],[0,-1],[1,0]]:
+                nr,nc = r+dx, c+dy
 
-            # 상하좌우탐색
-            find = (dfs(r+1,c, idx+1) or dfs(r,c+1,idx+1) or dfs(r-1,c,idx+1) or dfs(r,c-1,idx+1))
-            
-            board[r][c] = tmp # 백트래킹: 탐색 후 원래 값으로 복원
-            return find
-            
+                if dfs(nr,nc,idx+1):
+                    return True
+            visited.remove((r,c))
+
 
         for r in range(n):
             for c in range(m):
                 if dfs(r,c,0):
                     return True
         return False
-        
