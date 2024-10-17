812dd34811b11897001c4c86d8b4ae864ca6f6bd
queenJiJi <lovebellaberry52@gmail.com>
2024-10-17 20:34:42 +0900

    Time: 3 ms (100%), Space: 11.9 MB (33%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,31 +1,23 @@
 class Solution(object):
     def permute(self, nums):
-        # def backtrack(cur):
-        #     # base case
-        #     if len(cur) == len(nums):
-        #         ans.append(cur[:]) 
-        #         return
-            
-        #     for num in nums:
-        #         if num not in cur:
-        #             cur.append(num)
-        #             backtrack(cur)
-        #             cur.pop()
-        # ans = []
-        # backtrack([])
-        # return ans
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        answer = []
 
-        ans,result =[],[]
-        n = len(nums)
-        def backtrack():
-            if n == len(ans):
-                result.append(ans[:])
+        def backtrack(tmp):
+            if len(nums) == len(tmp):
+                answer.append(tmp[:])
                 return
-            
-            for i in range(n):
-                if nums[i] not in ans:
-                    ans.append(nums[i])
-                    backtrack()
-                    ans.pop()
-            return result
-        return backtrack()        
+
+            for i in range(len(nums)):
+                if nums[i] not in tmp:
+                    tmp.append(nums[i])
+                    backtrack(tmp)
+                    tmp.pop()    
+
+        backtrack([])
+        return answer
+
+
