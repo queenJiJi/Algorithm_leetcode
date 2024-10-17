3d47b0ca5bb4ff4cce2c8934ac5f82d58389ff70
queenJiJi <lovebellaberry52@gmail.com>
2024-10-17 21:05:39 +0900

    Time: 2036 ms (5.16%), Space: 61.5 MB (5.75%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -2,5 +2,7 @@ from itertools import permutations
 class Solution(object):
     def getPermutation(self, n, k):
         return ''.join(map(str,list(permutations(range(1,n+1),n))[k-1]))
+        
+       
 
        
