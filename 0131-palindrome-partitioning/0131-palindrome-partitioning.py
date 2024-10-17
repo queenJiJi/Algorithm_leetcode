003de6f97e11e10066ec45c8b02dfa1a8ffb3818
queenJiJi <lovebellaberry52@gmail.com>
2024-10-17 21:20:21 +0900

    Time: 493 ms (46.82%), Space: 31 MB (37.34%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,26 +1,16 @@
 class Solution(object):
     def partition(self, s):
+        ans,result = [],[]
         n = len(s)
-        ans,result = [], []  
-        answer = []
         def backtrack(start):
-            # base case
             if start == n:
                 result.append(ans[:])
-
+            
             for i in range(start,n):
-                ans.append(s[start:i+1])
-                backtrack(i+1)
-                ans.pop()
+                substr = s[start:i+1]
+                if substr == substr[::-1]: # check if palindrome
+                    ans.append(substr)
+                    backtrack(i+1)
+                    ans.pop()
             return result
-        
-        def isPalindrome(sub):
-            return sub == sub[::-1]
-        
-        ret = backtrack(0)
-
-        if ret: # check if palindrome
-            for ch in ret:
-                if all(isPalindrome(c) for c in ch):
-                    answer.append(ch)
-        return answer
+        return backtrack(0)
