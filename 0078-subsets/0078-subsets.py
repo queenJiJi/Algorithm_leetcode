9d49ade87ef42ddcb6b13b893805bd42565c95d3
queenJiJi <lovebellaberry52@gmail.com>
2024-10-17 20:46:21 +0900

    Time: 0 ms (100%), Space: 11.7 MB (66.09%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,7 +1,19 @@
-from itertools import combinations
 class Solution(object):
     def subsets(self, nums):
-        answer= []
-        for i in range(len(nums)+1):
-            answer.extend(list(combinations(nums, i)))
-        return answer        
+        answer = []
+
+        def backtrack(tmp):
+            answer.append(tmp[:])
+
+            if len(tmp) == len(nums):
+                return
+
+            for num in nums:
+                if tmp and num <= max(tmp):
+                    continue
+                tmp.append(num)
+                backtrack(tmp)
+                tmp.pop()
+
+        backtrack([])
+        return answer
