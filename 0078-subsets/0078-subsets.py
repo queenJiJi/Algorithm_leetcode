b06d11e51fd34389d0b4fbb12b354b17f644fc21
queenJiJi <lovebellaberry52@gmail.com>
2025-10-23 01:42:52 +0900

    Time: 0 ms (100%), Space: 12.8 MB (21.6%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,18 +2,16 @@ class Solution(object):
     def subsets(self, nums):
         answer = []
 
-        def backtrack(tmp):
+        def backtrack(start, tmp):
             answer.append(tmp[:])
 
             if len(tmp) == len(nums):
                 return
 
-            for num in nums:
-                if tmp and num <= max(tmp):
-                    continue
-                tmp.append(num)
-                backtrack(tmp)
+            for i in range(start,len(nums)):
+                tmp.append(nums[i])
+                backtrack(i+1,tmp)
                 tmp.pop()
-
-        backtrack([])
+        backtrack(0,[])
         return answer
+                
