d6de0dc919f3facfb41d023a357e4d679fa33958
queenJiJi <lovebellaberry52@gmail.com>
2025-11-03 16:51:27 +0900

    Time: 4 ms (29.68%), Space: 13.8 MB (16.57%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,22 +1,15 @@
-from collections import deque
 class Solution(object):
     def canVisitAllRooms(self, rooms):
-        """
-        :type rooms: List[List[int]]
-        :rtype: bool
-        """
-        q = deque()
-        q.append(0)
-        visit = [False]* len(rooms)
-        visit[0] = True
+        visited = [False]*len(rooms) 
 
-        while q:
-            cur_room = q.popleft()
+        def dfs(room):
+            visited[room] = True
 
-            for next_room in rooms[cur_room]:
-                if not visit[next_room]:
-                    visit[next_room] = True
-                    q.append(next_room)
-        return True if all(visit) else False
-
+            for next_room in rooms[room]:
+                if not visited[next_room]:
+                    visited[next_room] = True
+                    dfs(next_room)
         
+        dfs(0)
+
+        return True if all(visited) else False
