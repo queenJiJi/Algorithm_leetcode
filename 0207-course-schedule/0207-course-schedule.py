f34553cf82ad92a86b3a5fe95621943419fd3c9f
queenJiJi <lovebellaberry52@gmail.com>
2025-11-04 21:15:32 +0900

    Time: 5 ms (87.61%), Space: 13.2 MB (95.67%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,30 +1,28 @@
-from collections import deque
 class Solution(object):
+    from collections import deque
     def canFinish(self, numCourses, prerequisites):
-        graph = [[] for _ in range(numCourses)]
         visited = []
-        indegree= [0]*numCourses
-        for v,u in prerequisites:
-            graph[u].append(v)
-            indegree[v]+=1
-        
-        q = deque()
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0]*numCourses
 
-        for node in range(numCourses):
-            if indegree[node] == 0:
-                q.append(node)
+        for u,v in prerequisites:
+            graph[v].append(u)
+            indegree[u] += 1
+
+        q = deque()
+        
+        for v in range(numCourses):
+            if indegree[v] == 0:
+                q.append(v)
 
         while q:
             cur_v = q.popleft()
             visited.append(cur_v)
 
             for next_v in graph[cur_v]:
-                indegree[next_v] -=1
+                indegree[next_v] -= 1
 
                 if indegree[next_v] == 0:
                     q.append(next_v)
-        return True if len(visited) == numCourses else False 
+        return True if len(visited) == numCourses else False
         
-
-    
-       
