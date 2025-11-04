b28eb94334ec4fad65eb0067d9103316a1364b3b
queenJiJi <lovebellaberry52@gmail.com>
2025-11-04 20:32:01 +0900

    Time: 404 ms (18.15%), Space: 18.6 MB (99.98%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,27 +1,24 @@
-import collections, heapq
-class Solution(object):
-    def networkDelayTime(self, times, n, k):
-        # 인접리스트로 변환
-        graph = defaultdict(list)
+import heapq
+from collections import defaultdict
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)   
         for time in times:
             graph[time[0]].append((time[2],time[1]))
-
-        # 다익스트라 알고리즘 적용
-        pq = [(0,k)]
-        costs = {} # 비용을 업데이트 해주며 일종의 방문표시
         
+        pq =[]
+        costs = {}
+        heapq.heappush(pq, (0,k))
+
         while pq:
             cur_cost,cur_v = heapq.heappop(pq)
-
             if cur_v not in costs:
                 costs[cur_v] = cur_cost
-
-                for cost,next_v in graph[cur_v]:
+                for cost, next_v in graph[cur_v]:
                     next_cost = cur_cost + cost
-                    heapq.heappush(pq,(next_cost,next_v))
-        
-        # 전체노드를 방문했나 확인
-        for node in range(1,n+1):
-            if node not in costs:
+                    heapq.heappush(pq,(next_cost, next_v))
+        for i in range(1,n+1):
+            if i not in costs:
                 return -1
+
         return max(costs.values())
