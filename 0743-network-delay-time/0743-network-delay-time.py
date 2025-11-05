2a84c9e78d8136d134692c4ac9fcf4f5e7a0d1a8
queenJiJi <lovebellaberry52@gmail.com>
2025-11-05 18:27:15 +0900

    Time: 414 ms (12.9%), Space: 14.8 MB (25.49%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -8,31 +8,28 @@ class Solution(object):
             graph[u].append((w,v))
             # graph[w].append((w,u))
 
-        costs = [float('inf')]*(n+1)
-        costs[k] = 0
-        # costs = {}
+        # costs = [-1]*(n+1)
+        # costs[k] = 0
+        costs ={}
         pq = []
         heapq.heappush(pq,(0,k))
 
         while pq:
             cur_cost, cur_v = heapq.heappop(pq)
 
-            # if cur_v not in costs:
-            #     costs[cur_v] = cur_cost
-            # if cur_cost > costs[cur_v]:
-            #     continue
-
-            for cost, next_v in graph[cur_v]:
-                next_cost = cost+cur_cost
-                if next_cost < costs[next_v]:
-                    costs[next_v] = next_cost
+            if cur_v not in costs:
+                costs[cur_v] = cur_cost
+            
+                for cost, next_v in graph[cur_v]:
+                    next_cost = cost+cur_cost
+                    # if next_cost < costs[next_v]:
+                    #     costs[next_v] = next_cost
                     heapq.heappush(pq,(next_cost, next_v))
 
-        # for node in range(1,n+1):
-        #     if node not in costs:
-        #         return -1
-        answer = max(costs[1:])
-        return -1 if answer == float('inf') else answer
+        for node in range(1,n+1):
+            if node not in costs:
+                return -1
+        return max(costs.values())
         # return costs[n]
 
         
