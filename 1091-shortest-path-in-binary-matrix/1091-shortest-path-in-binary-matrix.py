e7cdeac17e059e1c416e8daad1224f797ebca4b1
queenJiJi <lovebellaberry52@gmail.com>
2025-11-05 16:47:54 +0900

    Time: 327 ms (28.93%), Space: 13.7 MB (39.92%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,35 +1,26 @@
 class Solution(object):
     from collections import deque
     def shortestPathBinaryMatrix(self, grid):
-            n = len(grid)
+        visited = set()
+        q = deque()
+        q.append((0,0,1))
+        n = len(grid)
+        visited.add((0,0))
 
-            if grid[0][0]!= 0 or grid[n-1][n-1]!=0:
-                    return -1
-
-            q = deque()
-            q.append((0,0,1))
-            visited = [[False]*n for _ in range(n)]
-            # check = [[-2]*n for _ in range(n)]
-            # check[0][0] = 1
-            visited[0][0] = True
-
-            while q:
-                x,y,path = q.popleft()
-                if (x,y)==(n-1,n-1):
-                    # print(visited)
-                    # print(check)
-                    return path
-
-                for dr,dc in [[-1,0],[0,-1],[0,1],[1,0],[-1,1],[1,-1],[1,1],[-1,-1]]:
-                    nr,nc=x+dr, y+dc
-
-                    if 0<=nr<n and 0<=nc<n and not visited[nr][nc] and grid[nr][nc]==0:
-                        q.append((nr,nc,path+1))
-                        visited[nr][nc] = True
-                        # check[nr][nc] = path
-                        # print(path)
-            # print(check)
+        if grid[0][0] != 0 or grid[n-1][n-1] !=0:
             return -1
-
         
+        while q:
+            cur_r,cur_c, path = q.popleft()
+
+            if (cur_r,cur_c) == (n-1,n-1):
+                return path
+
+            for dx,dy in [[-1,0],[1,0],[0,1],[0,-1],[-1,1],[1,-1],[-1,-1],[1,1]]:
+                nr,nc = cur_r+dx, cur_c+dy
+
+                if 0<=nr<n and 0<=nc<n and (nr,nc) not in visited and grid[nr][nc]==0:
+                    q.append((nr,nc,path+1))
+                    visited.add((nr,nc))
+        return -1
         
