afda07ba7a44a33870548526e337c205ff2d3e9b
queenJiJi <lovebellaberry52@gmail.com>
2025-11-05 16:09:41 +0900

    Time: 27 ms (19.52%), Space: 13.7 MB (47.9%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.py
@@ -1,19 +1,19 @@
 class Solution(object):
     def longestValidParentheses(self, s):
+        stack = [-1]
         answer = 0
+
         if not s:
             return answer
 
-        stack = [-1]
-
         for i in range(len(s)):
-            if s[i]== '(':
+            if stack and s[i] == '(':
                 stack.append(i)
             else: # s[i] == ')'
                 stack.pop()
-                if stack: # stack이 비어있지 않다면
+                if stack: # 스택이 비어있지 않다면
                     answer = max(answer, i-stack[-1])
-                else: # stack이 비어있다면
+                else: # 스택이 비어있다면
                     stack.append(i)
         return answer
-                       
+        
