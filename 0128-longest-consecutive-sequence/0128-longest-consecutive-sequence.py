2d81b6ad472aab2308d6c0bef99110f5a073ca94
queenJiJi <lovebellaberry52@gmail.com>
2025-11-05 20:42:23 +0900

    Time: 47 ms (85.32%), Space: 27.5 MB (15.15%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,14 +1,23 @@
 class Solution(object):
     def longestConsecutive(self, nums):
-        hashset = set(nums)
-        count, answer = 0,0
-        for val in hashset:
-            if val-1 not in hashset:
-                count = 1
-                next = val+1
-                while next in hashset:
-                    count += 1
-                    next += 1 
-                answer = max(count, answer)
+        if not nums:
+            return 0
+
+        nums = sorted(set(nums)) # 중복제거 + 정렬
+
+        cnt = 1
+        answer = 1
+
+        for i in range(len(nums)-1):
+
+            if nums[i+1]-nums[i]== 1:
+                cnt+=1
+                # print('i',i,cnt)
+            else:
+                answer = max(cnt, answer)
+                cnt= 1 # 연속성 끊기면 다시 1로 초기화
+        answer = max(cnt, answer)
+
+        # print(answer)
         return answer
         
