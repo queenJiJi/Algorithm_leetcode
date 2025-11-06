6ea53fab5d6541ccf487c01d12ccbc8f9abfae1c
queenJiJi <lovebellaberry52@gmail.com>
2025-11-06 16:34:45 +0900

    Time: 17 ms (5.69%), Space: 12.4 MB (85.42%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,5 +1,29 @@
 class Solution(object):
     def uniquePaths(self, m, n):
+        dp = [[1]*n for _ in range(m)]
+        print(dp)
+        
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         # bottom-up
         # dp = [[-1] * n for _ in range(m)]
 
@@ -15,17 +39,17 @@ class Solution(object):
         # return dp[m-1][n-1]
 
         # top-down
-        memo = [[-1]*n for _ in range(m)]
+        # memo = [[-1]*n for _ in range(m)]
 
-        def dp(r,c):
-            if r==0 or c==0:
-                memo[r][c] = 1
+        # def dp(r,c):
+        #     if r==0 or c==0:
+        #         memo[r][c] = 1
             
-            if memo[r][c] == -1:
-                memo[r][c] = dp(r-1,c) + dp(r,c-1)
+        #     if memo[r][c] == -1:
+        #         memo[r][c] = dp(r-1,c) + dp(r,c-1)
 
-            return memo[r][c]
-        return dp(m-1,n-1)
+        #     return memo[r][c]
+        # return dp(m-1,n-1)
         
 
         
