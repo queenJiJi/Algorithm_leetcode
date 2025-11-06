9d00cb76765104c257c4ea9710ffbbbbf83a7c6e
queenJiJi <lovebellaberry52@gmail.com>
2025-11-06 16:50:25 +0900

    Time: 414 ms (13.01%), Space: 14.7 MB (53.49%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,39 +1,29 @@
 class Solution(object):
     import heapq
     from collections import defaultdict
+    
     def networkDelayTime(self, times, n, k):
+        costs = {}
+        pq = []
+        heapq.heappush(pq,(0,k))
         graph = defaultdict(list)
 
         for u,v,w in times:
             graph[u].append((w,v))
-            # graph[w].append((w,u))
-
-        costs = [float('inf')]*(n+1)
-        costs[k] = 0
-        # costs = {}
-        pq = []
-        heapq.heappush(pq,(0,k))
 
         while pq:
             cur_cost, cur_v = heapq.heappop(pq)
 
-            # if cur_v not in costs:
-            #     costs[cur_v] = cur_cost
-            # if cur_cost > costs[cur_v]:
-            #     continue
+            if cur_v not in costs:
+                costs[cur_v] = cur_cost
+            
+                for cost, next_v in graph[cur_v]:
+                    next_cost = cost+cur_cost
+                    heapq.heappush(pq, (next_cost, next_v))
 
-            for cost, next_v in graph[cur_v]:
-                next_cost = cost+cur_cost
-                if next_cost < costs[next_v]:
-                    costs[next_v] = next_cost
-                    heapq.heappush(pq,(next_cost, next_v))
-
-        # for node in range(1,n+1):
-        #     if node not in costs:
-        #         return -1
-        answer = max(costs[1:])
-        return -1 if answer == float('inf') else answer
-        # return costs[n]
-
+        for i in range(1,n+1):
+            if i not in costs:
+                return -1        
+        return max(costs.values()) 
         
         
