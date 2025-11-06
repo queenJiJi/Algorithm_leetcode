55026cc120e9d685bf0def55adcb0d6934db76fd
queenJiJi <lovebellaberry52@gmail.com>
2025-11-06 17:03:56 +0900

    Time: 394 ms (20.5%), Space: 14.8 MB (53.49%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,7 +1,6 @@
 class Solution(object):
     import heapq
     from collections import defaultdict
-    
     def networkDelayTime(self, times, n, k):
         costs = {}
         pq = []
@@ -9,21 +8,21 @@ class Solution(object):
         graph = defaultdict(list)
 
         for u,v,w in times:
-            graph[u].append((w,v))
+            graph[u].append((w,v)) 
 
         while pq:
             cur_cost, cur_v = heapq.heappop(pq)
 
             if cur_v not in costs:
                 costs[cur_v] = cur_cost
-            
+
                 for cost, next_v in graph[cur_v]:
                     next_cost = cost+cur_cost
-                    heapq.heappush(pq, (next_cost, next_v))
-
+                    heapq.heappush(pq,(next_cost, next_v))
         for i in range(1,n+1):
             if i not in costs:
-                return -1        
-        return max(costs.values()) 
+                return -1
+        return max(costs.values())
+
         
         
